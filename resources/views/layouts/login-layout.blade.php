69e9a930dd103221dc99ff3f30ae9cf15a7d7080
Seth Bongo <rbongo581@gmail.com>
2026-02-02 00:20:14 +0800

    done. i might implement components

--- resources/views/layouts/login-layout.blade.php
+++ resources/views/layouts/login-layout.blade.php
@@ -2,52 +2,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <x-layout/>
-
+    <x-link/>
     <title>@yield('title', '')</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login-layout.css') }}">
 
-<style>
-body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-#forms {
-    background-color: white !important;
-    border-radius: 10px;
-    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
-    padding: 40px;
-    max-width: 400px;
-}
-
-#forms input[type="text"],
-#forms input[type="email"],
-#forms input[type="password"] {
-    width: 100%;
-    padding: 12px 15px;
-    border: 2px solid #000000;
-    border-radius: 5px;
-    font-size: 14px;
-}
-
-#forms button {
-    width: 100%;
-    padding: 12px;
-    color: rgb(0, 0, 0);
-    border: 1px black solid;
-    border-radius: 5px;
-    font-size: 16px;
-    font-weight: 600;
-    cursor: pointer;
-    background-color: white
-}
-
-#forms button:hover {
-            background-color: #bebaba;
-}
-    
-</style>
+    {{-- @vite(['resources/css/app.css', 'resources/css/login-layout.css']) --}}
 </head>
 
 <body>
